342ca8594756b6d8cfc958118dfe5f3f8b0c563f
Kalle <karlfock@gmail.com>
2014-02-11 18:51:46 +0100

    fix for slickgrid dependency

--- requireConfig.js
+++ requireConfig.js
@@ -1,19 +1,19 @@
 var require = {
-    baseUrl : ".",            
+    baseUrl: ".",
     paths: {
-        handlebars: "vendor/handlebars",   
-        jquery : "vendor/jquery-1.8.3",
+        handlebars: "vendor/handlebars",
+        jquery: "vendor/jquery-1.8.3",
         "jquery-ui": "vendor/jquery-ui-1.9.2",
         "jquery-event-drag": "vendor/jquery.event.drag-2.0.min",
         "jquery-validate": "vendor/jquery.validate.min",
-        underscore : "vendor/underscore",
+        underscore: "vendor/underscore",
         slickCore: "vendor/jquery.SlickGrid-2.1/slick.core",
         slickGrid: "vendor/jquery.SlickGrid-2.1/slick.grid",
         form2js: "vendor/form2js"
     },
 
     // make these non-require modules global where required.
-    shim: {            
+    shim: {
         "jquery-ui": {
             exports: "jquery-ui",
             deps: ["jquery"]
@@ -21,6 +21,10 @@ var require = {
         "jquery-validate": {
             exports: "jquery-validate",
             deps: ["jquery", "jquery-ui"]
+        },
+        "jquery-event-drag": {
+            exports: "jquery-event-drag",
+            deps: ["jquery"]
         },
         "slickCore": {
             exports: "slickCore",
@@ -30,12 +34,12 @@ var require = {
             exports: "slickGrid",
             deps: ["jquery-event-drag"]
         },
-        "underscore" : {
-            exports : "_"
+        "underscore": {
+            exports: "_"
         },
-        "handlebars" : {
+        "handlebars": {
             deps: ["jquery"],
-            exports : "Handlebars"
+            exports: "Handlebars"
         },
         "form2js": {
             exports: "form2js"
